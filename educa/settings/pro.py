5cb92faa1780b0eb9c58ac8d8d42be4c79b9ff2a
syed <syed@testpress.in>
2024-09-16 18:47:01 +0530

    BugFix on Nginx config

--- educa/settings/pro.py
+++ educa/settings/pro.py
@@ -6,8 +6,6 @@ ADMINS = (
 )
 
 
-ALLOWED_HOSTS = ['*']
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -21,7 +19,11 @@ DATABASES = {
         }
     }
 }
-ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com', ]
 
 TIME_ZONE = 'UTC'
 USE_TZ = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+SECRET_KEY = 'django-insecure-6i+d8k@reflw7pz#cm9_s4)3waosi5tyo^)xwjk1m(wplweiu1'
